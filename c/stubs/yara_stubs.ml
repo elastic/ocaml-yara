e3b71e9b4a0dbd781c0488050344aa61bba865cc
Anurag Soni <anurag.soni@elastic.co>
2021-10-12 17:01:43 -0400

    dont seal the structs

--- c/stubs/yara_stubs.ml
+++ c/stubs/yara_stubs.ml
@@ -50,7 +50,7 @@ module Definition (F : Cstubs.FOREIGN) = struct
     let integer = field t "integer" int64_t
     let type_ = field t "type" meta_type
     let flags = field t "flags" int32_t
-    let () = seal t
+    (* let () = seal t *)
   end
 
   module Yr_namespace = struct
@@ -59,7 +59,7 @@ module Definition (F : Cstubs.FOREIGN) = struct
 
     let name = field t "name" string
 
-    let () = seal t
+    (* let () = seal t *)
   end
 
   module Yr_rule = struct
@@ -69,7 +69,7 @@ module Definition (F : Cstubs.FOREIGN) = struct
     let identifier = field t "identifier" string
     let namespace = field t "ns" (ptr Yr_namespace.t)
 
-    let () = seal t
+    (* let () = seal t *)
   end
 
   module Yr_rules = struct
@@ -80,7 +80,7 @@ module Definition (F : Cstubs.FOREIGN) = struct
     let string_count = field t "num_strings" uint32_t
     let namespace_count = field t "num_namespaces" uint32_t
 
-    let () = seal t
+    (* let () = seal t *)
 
     let destroy = foreign "yr_rules_destroy" (ptr t @-> returning void)
   end
